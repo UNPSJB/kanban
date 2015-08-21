caa36c9a2363f539db0ddf6146c45451e95bb624
Diego van Haaster <diegomvh@gmail.com>
2015-08-21 09:16:50 -0300

    Crear migracion

---
+++ kanban/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Columna',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tablero',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tarjeta',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+            ],
+        ),
+    ]
